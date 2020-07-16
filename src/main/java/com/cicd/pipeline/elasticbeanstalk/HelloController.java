9bacf065e1f1d5416e6a14b27c4590a61c545bcf
Esposito <rodrespo@a483e7840cdd.ant.amazon.com>
2020-07-16 17:47:17 -0300

    AWS BeanStalk example -add GetMapping for Hello Controller

--- src/main/java/com/cicd/pipeline/elasticbeanstalk/HelloController.java
+++ src/main/java/com/cicd/pipeline/elasticbeanstalk/HelloController.java
@@ -1,11 +1,14 @@
 package com.cicd.pipeline.elasticbeanstalk;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/hello")
 @RestController
 public class HelloController {
+
+    @GetMapping
     public String hello(){
         return "Hello CapStone";
     }
